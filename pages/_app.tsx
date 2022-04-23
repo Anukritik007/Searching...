14504e17c93708bafc82145142b55c81322090d9
Anukritik007 <anukritik77@gmail.com>
2022-04-23 17:01:25 +0530

    Project base setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Resourceful</title>
+        <meta
+          name="description"
+          content="One-stop finder for resources of any kind posted by community of the like."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
