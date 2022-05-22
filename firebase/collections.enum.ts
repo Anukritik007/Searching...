3992a1e19085f40d39f4a0200ef12bb4a115f4f7
mayank <mayank8005@gmail.com>
2022-05-22 20:31:35 +0530

    add firestore

---
+++ firebase/collections.enum.ts
@@ -0,0 +1,3 @@
+export enum COLLECTIONS {
+    RESOURCES = 'Resources'
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
+import React, { useEffect } from 'react';
 import type { NextPage } from "next";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "../styles/Home.module.css";
 import { ResourceCard } from "../components/molecules/ResourceCard/ResourceCard";
 import ThemeSwitcher from "../components/atoms/ThemeSwitcher";
 import { initStore } from "../redux/store";
+import { collection, getDocs } from 'firebase/firestore';
 import { IResource, IAppState } from "../interfaces/Interfaces";
+import firebase from "../firebase/firebase";
+import { COLLECTIONS } from '../firebase/collections.enum';
+import { addNewResource } from '../redux/resources/resourcesActions';
 // import { getAllResources } from "../redux/resources/resourcesActions";
 
 const Home: NextPage = () => {
   const { data } = useSelector((state: IAppState) => state.resources);
+  const dispatch = useDispatch()
+  
+  const getResources = async () => {
+    const result = await getDocs(collection(firebase.fireStore, COLLECTIONS.RESOURCES));
+    const resources: Array<IResource> = []
+    result.forEach((doc) => {
+      resources.push(doc.data() as IResource);
+    });  
+    // TODO: change logic in future
+    resources.forEach((resource) => {
+      dispatch(addNewResource(resource))
+    })
+  }
+
+  useEffect(() => {
+    getResources();
+  }, [])
 
   return (
     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 dark:from-indigo-500 dark:to-black">
